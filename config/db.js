3db2410c37d9b15ba79fda6cf66194ce275708e8
justinepeterson <jmahinyila@gmail.com>
2020-01-04 02:35:09 -0500

    Database connected

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const db = config.get('mongoURL');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
+        console.log('Database connected')
+    } catch (error) {
+        console.log(error.message)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB;
