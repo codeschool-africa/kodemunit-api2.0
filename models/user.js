376e86bef4b98c5a4712043f5091d03934fec7be
justinepeterson <jmahinyila@gmail.com>
2020-01-07 10:17:32 -0500

    User registration confirmation via email

--- models/user.js
+++ models/user.js
@@ -21,6 +21,7 @@ const UserSchema = new mongoose.Schema({
     avatar: {
         type: String
     },
+    confirmed: { type: Boolean, default: false },
     date: {
         type: Date,
         default:Date.now
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,12 +2,40 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const dotenv = require("dotenv");
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator/check');
+const nodemailer = require("nodemailer");
+
 
 const User = require('../../models/user');
+dotenv.config();
+const secret = config.get('jwtSecret');
+//transporter 
+let transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: "jmahinyila@gmail.com", // generated ethereal user
+    pass: "uiocfzcnzhouhrca" // generated ethereal password
+  }
+});
 
+// @route    POST api/users
+// @desc     User email confirmation
+// @access   Public
+router.get('/confirmation/:token', async (req, res) => {
+    const user = jwt.verify(req.params.token, secret)
+    console.log(user)
+    try {
+        const { user: { id } } = jwt.verify(req.params.token, secret);
+        await User.update({ confirmed: true }, { where: { id } });
+        res.send('User registration confirmed')
+    } catch (errors) {
+        console.log(`Email Confrimation Error ${errors}`)
+        res.send('Email confirmation Error')
+    }
+})
 // @route    POST api/users
 // @desc     Register user
 // @access   Public
@@ -32,7 +60,7 @@ router.post(
             return res.status(400).json({ errors: errors.array() });
         }
 
-        const { firstname,secondname, email, password } = req.body;
+        const { firstname,secondname,confirmed, email, password } = req.body;
 
         try {
             let user = await User.findOne({ email });
@@ -53,6 +81,7 @@ router.post(
                 firstname,
                 secondname,
                 email,
+                confirmed,
                 avatar,
                 password
             });
@@ -69,11 +98,29 @@ router.post(
                 }
             };
 
+        
             jwt.sign(
                 payload,
-                config.get('jwtSecret'),
+                secret,
                 { expiresIn: 2592000 },
                 (err, token) => {
+                    transporter.sendMail(
+                      {
+                        to: user.email,
+                        subject: "Kodemunit.com Email Confimation",
+                            html: `Thank you for Registering with Kodemunit.com <br>
+                        Please click this link to confirm your email address: <a href="http://localhost:5000/api/users/confirmation/${token}">Link</a>`
+                      },
+                      (error) => {
+                        if (error) {
+                          console.log(error);
+                          res.end("error");
+                        } else {
+                          console.log("check your email");
+                          res.end("check your email");
+                        }
+                      }
+                    );
                     if (err) throw err;
                     res.json({ token });
                 }
