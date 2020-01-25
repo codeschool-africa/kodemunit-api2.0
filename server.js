79338ab06dc6a93612f6c645df53b9991af514a6
Benedict Steven <48621248+bmsteven@users.noreply.github.com>
2020-01-26 01:28:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const app = express();
 
 //solving corse errors 
 app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-//     next();
-    res.header("Access-Control-Allow-Origin", "http://kodemunit.herokuapp.com"); // update to match the domain you will make the request from
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header("Access-Control-Allow-Origin", "http://kodemunit.herokuapp.com.tld"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
